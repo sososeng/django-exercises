44cba37f1e107631c76d1cc641b4f57d4a07918d
Sokhun Seng <sososeng@gmail.com>
2017-05-09 12:13:40 -0500

    testing uwsgi

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+'162.243.214.212',
+]
 
 
 # Application definition
